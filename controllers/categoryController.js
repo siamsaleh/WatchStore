c73af356606fb40f0ccfae402420b3f49d1c10f8
siamsaleh <siamsaleh9@gmail.com>
2023-08-21 00:35:13 +0600

    Add Category

---
+++ controllers/categoryController.js
@@ -0,0 +1,35 @@
+import categoryModel from "../models/categoryModel.js";
+import slugify from "slugify";
+
+export const createCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).send({ message: "Name is not found" });
+    }
+    const existingCategory = await categoryModel.findOne({ name });
+    if (existingCategory) {
+      return res.status(200).send({
+        success: true,
+        message: "Category Already Exists",
+      });
+    }
+
+    const category = await new categoryModel({
+      name,
+      slug: slugify(name),
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "new category created",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "error in category",
+    });
+  }
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,1 +1,15 @@
-import { Express } from "express";
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import { createCategoryController } from "../controllers/categoryController.js";
+
+const router = express.Router();
+
+//routes
+router.post(
+  "/create-category",
+  requireSignIn,
+  isAdmin,
+  createCategoryController
+);
+
+export default router;
